8931bbecae377008aeb091d2536b6e76f2c3b7f4
Demis Bellot <demis.bellot@gmail.com>
2018-08-20 01:47:50 -0400

    wordings

--- client/index.ts
+++ client/index.ts
@@ -9,7 +9,7 @@ document.querySelector("#Name")!.addEventListener("input", async e => {
     const request = new Hello();
     request.name = value;
     const response = await client.get(request);
-    // const response = await client.get<HelloResponse>(`/hello/${request.name}`); // call /hello/_name.html instead
+    // const response = await client.get<HelloResponse>(`/hello/${request.name}`); //call /hello/_name.html API
     result.innerHTML = response.result;
   } else {
     result.innerHTML = "";
